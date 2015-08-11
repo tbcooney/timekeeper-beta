0a13059773aebc18ff3a13d1e920cd0251e25a5d
tbcooney <taylor.cooney@vegastudios.ca>
2015-08-10 20:56:32 -0400

    Added an association between User & documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,13 +1,37 @@
 class DocumentsController < ApplicationController
+  before_action :find_document, only: [:show, :edit, :update, :destroy]
+
   def index
+    @documents = Document.where(user_id: current_user)
   end
 
   def new
+    @document = current_user.documents.build
   end
 
   def create
+    @document = current_user.documents.build(document_params)
+
+    if @document.save
+      redirect_to documents_path, notice: "The document #{@document.name} has been uploaded."
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @document = Document.find(params[:id])
+    @document.destroy
+    redirect_to documents_path, notice:  "The document #{@document.name} has been deleted."
+  end
+
+  private
+
+  def find_document
+    @document = Document.find(params[:id])
+  end
+
+  def document_params
+    params.require(:document).permit(:name, :attachment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'documents/index'
-
-  get 'documents/new'
-
-  get 'documents/create'
-
-  get 'documents/destroy'
 
   devise_for :users
   get 'welcome/index'
-  resources :notes
+  resources :notes, :documents
 
   authenticated :user do
     root 'notes#index', as: "authenticated_root"
