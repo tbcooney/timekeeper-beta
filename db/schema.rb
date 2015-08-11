53639c537c649e9f9c1bc4fa32067eb646578e73
tbcooney <taylor.cooney@vegastudios.ca>
2015-08-10 20:08:25 -0400

    Generated Documents model, uploader & controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808014208) do
+ActiveRecord::Schema.define(version: 20150811000634) do
+
+  create_table "documents", force: :cascade do |t|
+    t.string   "name"
+    t.string   "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
